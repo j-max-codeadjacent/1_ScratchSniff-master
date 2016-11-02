568c26a78477bd0981e7cd4132953c6c34428fcd
j-max <jmax.barry@gmail.com>
2016-11-02 17:43:43 -0500

    Color Change Brains
    Before changing divs to responsive

---
+++ 1_ScratchSniff-master/js/main.js
@@ -0,0 +1,34 @@
+//js on load, brains flash colors
+
+//$(document).ready(function() {})
+
+$("#first-brain").on({'click':function(){
+	$('#first-brain').attr('src', 'images/Brain/brain-red.png')
+	}
+});
+
+$("#second-brain").on({'click':function(){
+	$('#second-brain').attr('src', 'images/Brain/brain-yellow.png')
+	}
+});
+
+$("#third-brain").on({'click':function(){
+	$('#third-brain').attr('src', 'images/Brain/brain-green.png')
+	}
+});
+
+$("#fourth-brain").on({'click':function(){
+	$('#fourth-brain').attr('src', 'images/Brain/brain-blue.png')
+	}
+});
+
+$("#last-brain").on({'click':function(){
+	$('#last-brain').attr('src', 'images/Brain/brain-purple.png')
+	}
+});
+
+/*can't get this to work, so I'll sue fixed for now.
+$("#right-side-bar").scroll(function(){
+	$(this).css('top', $(this).scrollTop());
+});
+*/
