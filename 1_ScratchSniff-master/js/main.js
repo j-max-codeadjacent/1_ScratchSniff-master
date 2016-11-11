9965e296d5027355c6d522106a2e463f6cd035f0
j-max <jmax.barry@gmail.com>
2016-11-11 15:23:46 -0600

    Before Realistic Brain
    Animation works fairly well. No squiggly lines yet.

--- 1_ScratchSniff-master/js/main.js
+++ 1_ScratchSniff-master/js/main.js
@@ -1,6 +1,57 @@
+//all in two seconds//
+//after clicking the link, trigger keyframe which moves hand up and then back and forth to imitate scratching.//
+
+
+ //delay removing the class so the link is clickable again. I'm not sure how this code works.
+$(".nav-link").on("click", function(){
+	var el     = $(this).siblings(".hand"),  
+		     newone = el.clone(true);
+		           
+		 el.before(newone);
+	        
+	 $(el).remove();
+});
+
+
+$(".nav-link").on("click", function(){
+	
+
+	$(this).siblings(".hand").removeClass("animate");
+	$(this).siblings(".hand").addClass("animate");
+	//this works with .nav-link but not nav-link a//
+	//delay the link so the animation can run//
+ 	var href = $("a", this).attr('href');
+	setTimeout(function() {window.location = href}, 2000);
+       return false;
+
+      
+});
+
+
+
+
+//smell line goes up into nose//
+
+
+//brain lights up//
+//user is brought to that section of the page//
+
+
 //js on load, brains flash colors
 
-//$(document).ready(function() {})
+
+
+//When scroll reaches #about author, replace proust with Virginia's image*/
+
+window.onscroll = function() {authorPhoto()};
+function authorPhoto() {
+    if (document.body.scrollTop > 500) {
+    	$("img#proust").toggleClass("hidden");
+        $("img#barry").toggleClass("show");
+    }
+
+};
+
 
 $("#first-brain").on({'click':function(){
 	$('#first-brain').attr('src', 'images/Brain/brain-red.png')
