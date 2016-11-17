03dc12915d1375a7fe24860f03bf18dfb8fae386
J. Max Barry <jmax.barry@gmail.com>
2016-11-17 13:49:02 -0600

    With Right Column Images
    Just added the neuron image.  Three columns now.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,135 @@
+//all in two seconds//
+
+/*
+$(".nav-link").on("click", function(){
+	var el = $(this).siblings(".hand"); 
+	var newone = el.clone(true);
+	 el.before(newone); 
+     el.remove();
+
+     var em= $("#smell-line");
+     var newEm = em.clone(true);
+     em.before(newEm);
+     em.remove();
+ });
+ */
+  	
+       //after clicking the link, trigger keyframe which moves hand up and then back and forth to imitate scratching.//
+
+
+
+ //delay removing the class so the link is clickable again. I'm not sure how this code works.
+
+
+
+$(".nav-link").on("click", function(){
+	var el = $(this).siblings(".hand");
+	
+	el.removeClass("animateScratch");
+	el.addClass("animateScratch");
+	
+	//this works with .nav-link but not nav-link a//
+	//delay the link so the animation can run//
+	var em = $("#smell-line");
+	em.removeClass("animateSniff");
+	em.addClass("animateSniff");
+
+
+	setTimeout(function() {
+		$("#brain").attr("src", "images/Brain/Pink-Brain.jpg");
+	}, 1400);
+
+	setTimeout(function() {
+		$("#brain").attr("src", "images/Brain/brain-white.png");
+	}, 3000);
+
+	setTimeout(function(){
+		em.removeClass("animateSniff");
+	}, 3000);
+	
+
+ 	var href = $("a", this).attr('href');
+	setTimeout(function() {
+		$('html, body').animate({scrollTop:$(href).position().top}, 'slow');
+		el.removeClass("animateScratch");
+	}, 1500);
+    
+    return false;
+  
+});
+
+
+
+
+
+
+
+//smell line goes up into nose//
+
+
+//brain lights up//
+//user is brought to that section of the page//
+
+
 //js on load, brains flash colors
 
-//$(document).ready(function() {})
 
-$("#first-brain").on({'click':function(){
-	$('#first-brain').attr('src', 'images/Brain/brain-red.png')
-	}
-});
 
-$("#second-brain").on({'click':function(){
-	$('#second-brain').attr('src', 'images/Brain/brain-yellow.png')
-	}
-});
+//When scroll reaches #about author, replace proust with Virginia's image*/
+function proustPic(){
+	$("#proust").addClass("show");
+}
 
-$("#third-brain").on({'click':function(){
-	$('#third-brain').attr('src', 'images/Brain/brain-green.png')
-	}
-});
+window.addEventListener("load", proustPic, false);
 
-$("#fourth-brain").on({'click':function(){
-	$('#fourth-brain').attr('src', 'images/Brain/brain-blue.png')
-	}
-});
+window.onscroll = function() {authorPhoto()};
+function authorPhoto() {
+   	 if (document.body.scrollTop < 1200) {
+    	$("#proust").addClass("show");
+        $("#barry").removeClass("show");
+    }
 
-$("#last-brain").on({'click':function(){
-	$('#last-brain').attr('src', 'images/Brain/brain-purple.png')
+    if (document.body.scrollTop > 1413) {
+    	$("#proust").removeClass("show");
+        $("#barry").addClass("show");
+        $("#hancock").removeClass("show");
+    }
+
+    if (document.body.scrollTop > 2711) {
+        $("#barry").removeClass("show");
+        $("#hancock").addClass("show");
+        $("#neuron").removeClass("show");
+    }
+
+
+    if (document.body.scrollTop > 3379) {
+        $("#barry").removeClass("show");
+        $("#hancock").removeClass("show");
+        $("#neuron").addClass("show");
+    }
+
+    //added nav bar to the scroll function
+    if (document.body.scrollTop > 380){
+		$("nav").addClass("nav-fixed");
 	}
+	if (document.body.scrollTop < 380){
+		$("nav").removeClass("nav-fixed");
+	}
+
+};
+
+/*window.onscroll=  function() {
+	if (document.body.scrollTop > 380){
+		$("nav").addClass("nav-fixed");
+	}
+	if (document.body.scrollTop < 380){
+		$("nav").removeClass("nav-fixed");
+	}
+
+};*/
+
+/*can't get this to work, so I'll sue fixed for now.
+$("#right-side-bar").scroll(function(){
+	$(this).css('top', $(this).scrollTop());
 });
+*/
